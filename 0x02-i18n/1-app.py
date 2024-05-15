44c40d61dc3925e1e3ff7738bff1be8cbda172b8
johhnMunene <Edwardsjohnny64@gmail.com>
2024-05-14 21:08:23 -0500

    . Basic Babel setup pep8

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,20 +6,23 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
 app.config.from_object(Config)
 
-@app.route('/', methods=['GET'], strict_slashes=False)
 
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """
     hello world
     """
     return render_template('1-index.html')
 
+
 if __name__ == '__main__':
     app.run(debug=True)
-
